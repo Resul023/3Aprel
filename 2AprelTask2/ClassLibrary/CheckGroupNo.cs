02b59e140172e2e14efe18e4321ca8b4e00fb0e1
Resul023 <73m6u7h@code.edu.az>
2022-04-03 13:10:15 -0700

    Task

---
+++ 2AprelTask2/ClassLibrary/CheckGroupNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public static class CheckGroupNo
+    {
+        public static bool CheckGroupNoo(this string groupNo) 
+        {
+            if (groupNo.Length == 4 && char.IsLetter(groupNo[0]) && char.IsDigit(groupNo[1]) && char.IsDigit(groupNo[2]) && char.IsDigit(groupNo[3]) )
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool CheckFullName(this string FullName) 
+        {
+            if (FullName.Split(" ").Length == 2 && FullName.Split(" ")[1] != "")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ 2AprelTask2/ClassLibrary/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Student
+    {
+        
+        public Student(string fullName, int Age,string groupNo)
+        {
+            FullName = fullName;
+            this.Age = Age;
+            GroupNo = groupNo;
+        }
+        private string _fullName { get; set; }
+
+        private string _groupNo;
+        public int Age { get; set; }
+
+        public string FullName 
+        {
+            get
+            { 
+                return this._fullName; 
+            }
+            private set 
+            {
+                if (value.Split(" ").Length == 2 && value.Split(" ")[1]!= "")
+                {
+                    this._fullName = value;
+                }
+                else
+                {
+
+                }
+            }
+        
+        }
+
+        public string GroupNo
+        {
+            get
+            {
+                return this._groupNo;
+            }
+            private set
+            {
+                if (value.Length == 4 && char.IsLetter(value[0]) && char.IsDigit(value[1]) && char.IsDigit(value[2]) && char.IsDigit(value[3]) )
+                {
+                    this._groupNo = value;
+                }
+                else
+                {
+                    
+                }
+
+            }
+
+        }
+        
+
+
+
+    }
+}
